4e5a89a5e1e84f51d6d83bdc8721463144dfd9fe
thewyolar <thewyolar@gmail.com>
2022-12-08 18:27:15 +0300

    Модифицированы скрипты. Добавлен скрипт для ползунка смены скорост.

--- CyberLife/wwwroot/js/site.js
+++ CyberLife/wwwroot/js/site.js
@@ -2,12 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function(){
+$(document).ready(function() {
+    moveStatistics();
     $('.nav-link').each(function () {
-        var location = window.location.href;
-        var link = this.href;
+        let location = window.location.href;
+        let link = this.href;
         if(location == link) {
             $(this).addClass("active");
         }
     });
 });
+
+let isStart = false;
+
+function toggle() {
+    let btn = document.getElementById("playBtn");
+    if(!isStart) {
+        isStart = true;
+        btn.classList.add("fa-pause");
+        btn.classList.remove("fa-play");
+        start();
+    } else {
+        isStart = false;
+        btn.classList.add("fa-play");
+        btn.classList.remove("fa-pause");
+        stop();
+    }
+}
