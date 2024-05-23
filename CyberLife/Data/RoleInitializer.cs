9f804b160f8096165313f7633edc48a35940a8dd
thewyolar <thewyolar@gmail.com>
2024-05-23 22:34:43 +0300

    Update docker files and RoleInitializer class

--- CyberLife/Data/RoleInitializer.cs
+++ CyberLife/Data/RoleInitializer.cs
@@ -6,26 +6,47 @@ namespace CyberLife.Data;
 
 public class RoleInitializer
 {
-    
+    private static readonly ILogger<RoleInitializer> _logger;
+
+    static RoleInitializer()
+    {
+        var loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+        _logger = loggerFactory.CreateLogger<RoleInitializer>();
+    }
+
     public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
     {
-        string adminEmail = "admin@gmail.com";
-        string password = await File.ReadAllTextAsync("OnConfiguring.txt");
+        string adminLogin = Environment.GetEnvironmentVariable("ADMIN_LOGIN");
+        string adminPassword = Environment.GetEnvironmentVariable("ADMIN_PASSWORD");
+        _logger.LogInformation("Admin login: {AdminLogin}, Admin password: {AdminPassword}", adminLogin, adminPassword);
+
         if (await roleManager.FindByNameAsync("Admin") == null)
         {
             await roleManager.CreateAsync(new IdentityRole("Admin"));
+            _logger.LogInformation("Admin role created.");
         }
         if (await roleManager.FindByNameAsync("User") == null)
         {
             await roleManager.CreateAsync(new IdentityRole("User"));
+            _logger.LogInformation("User role created.");
         }
-        if (await userManager.FindByNameAsync(adminEmail) == null)
+
+        if (adminLogin != null && adminPassword != null)
         {
-            User admin = new User { Email = adminEmail, UserName = adminEmail, EmailConfirmed = true};
-            IdentityResult result = await userManager.CreateAsync(admin, password);
-            if (result.Succeeded)
+            if (await userManager.FindByNameAsync(adminLogin) == null)
             {
-                await userManager.AddToRoleAsync(admin, "Admin");
+                User admin = new User { Email = adminLogin, UserName = adminLogin, EmailConfirmed = true };
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                    _logger.LogInformation("Admin user created and added to Admin role.");
+                }
+                else
+                {
+                    _logger.LogError("Failed to create admin user: {Errors}",
+                        string.Join(", ", result.Errors.Select(e => e.Description)));
+                }
             }
         }
     }
